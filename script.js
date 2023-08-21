4f1127f666001024c7c69be3bf08232fb0067f84
Patricia Santos <patricia.desenvolveti@gmail.com>
2023-08-21 20:53:35 -0300

    Adição de If Else no resultado do IMC

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function calcular(){
     let peso = document.getElementById('peso').value
     let altura = document.getElementById('altura').value
-    let calcular = Number(peso) / Number(altura) / Number(altura)
-    let resultado = document.getElementById('resultado').value = `${calcular.toFixed(2)} kg/m²`
+    let resultado = Number(peso) / Number(altura) / Number(altura)
+    let resultado_formatado = `${resultado.toFixed(2)} kg/m²`
+    document.getElementById('resultado').value = resultado_formatado
 
-
-    if (resultado <17.99){
-        resultado.innerHTML = `O seu IMC de ${resultado} está desnutrido`
+    if (resultado < 18.5){
+        document.getElementById('estado-nutricional').innerHTML = 'O seu estado nutricional deu: Desnutrido (a). Atenção! Vamos cuidar da alimentação?'
+        document.getElementById('resultado').style.backgroundColor = 'red'
+    } else if (resultado >= 18.5 && resultado < 25) {
+        document.getElementById('estado-nutricional').innerHTML = 'O seu estado nutricional deu: Eutrófico (a). Você está num bom caminho! Continue cuidando da sua alimentação.'
+        document.getElementById('resultado').style.backgroundColor = 'green'
     }
-    else{
-        
+    else if (resultado >= 25 && resultado < 30) { 
+        document.getElementById('estado-nutricional').innerHTML = 'O seu estado nutricional deu: Sobrepeso. Atenção! Posso te ajudar a fazer uma reeducação alimentar.'
+        document.getElementById('resultado').style.backgroundColor = 'orange'
+    }
+    else {
+        document.getElementById('estado-nutricional').innerHTML = 'O seu estado nutricional deu: Obesidade. Atenção! Posso te ajudar a fazer uma reeducação alimentar.'
+        document.getElementById('resultado').style.backgroundColor = 'red'
     }
 
   
